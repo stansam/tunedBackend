5206177c44de46d4acf7ffed18015a2b425cc411
stansam <stanleycmam122@gmail.com>
2026-05-29 06:19:24 +0300

    fix:socketio init wih gevent

--- wsgi.py
+++ wsgi.py
@@ -5,3 +5,11 @@ from tuned import create_app
 from tuned.extensions import socketio
 
 app = create_app()
+
+if __name__ == '__main__':
+    socketio.run(
+        app,
+        host='0.0.0.0',
+        port=5000,
+        debug=app.debug
+    )
